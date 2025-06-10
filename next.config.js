418b8ee4685de2ccc57b93b0e67309a0a3a30eea
adrian gherasim <adriangherasim1@gmail.com>
2025-06-10 23:08:39 +0300

    update infra

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/auth/:path*',
+//         destination: 'https://microservices1691714.westeurope.cloudapp.azure.com/auth/:path*',
+//       },
+//     ];
+//   },
+// };
+//
+// module.exports = nextConfig;
